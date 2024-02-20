5903f298ae9c64e60784833094a67217fcf8a2c2
Olive568 <luislabapis@gmail.com>
2024-02-20 22:13:04 +0800

    Changed the div for a background

--- Valentines.js
+++ Valentines.js
@@ -8,17 +8,6 @@ function hideCurrentPage(element) {
     element.style.display = "none";
 }
 function submitReasons() {
-    /*var reasons = document.querySelectorAll('input[name="reason"]:checked');
-    var selectedReasons = [];
-    reasons.forEach(function(checkbox) {
-        selectedReasons.push(checkbox.value);
-    });
-    
-    var emailBody = "Selected reasons:\n" + selectedReasons.join("\n");
-
-    var mailtoLink = "mailto:luislabapis@gmail.com?subject=Valentine's Day Reasons&body=" + encodeURIComponent(emailBody);
-
-    window.location.href = mailtoLink;*/
     document.getElementById("radioButtons").style.display = "none";
     document.getElementById("dates").style.display = "block";
 }
@@ -50,82 +39,15 @@ function submitDate()
     document.getElementById("dates").style.display = "none";
     document.getElementById("menu").style.display = "block";
 }
-function menu1(button) {
-        hideCurrentPage(button.parentElement);
-        document.getElementById("letter_1").style.display = "block";
-}
 function goback(button){
     hideCurrentPage(button.parentElement);
         document.getElementById("menu").style.display = "block";
 }
-function menu2(button) {
+function menuletters(button, letterNumber) {
     hideCurrentPage(button.parentElement);
-    document.getElementById("letter_2").style.display = "block";
+    document.getElementById("letter_" + letterNumber).style.display = "block";
 }
-function menu3(button) {
-    hideCurrentPage(button.parentElement);
-    document.getElementById("letter_3").style.display = "block";
-}
-function menu4(button) {
-    hideCurrentPage(button.parentElement);
-    document.getElementById("letter_4").style.display = "block";
-}
-function menu5(button) {
-    hideCurrentPage(button.parentElement);
-    document.getElementById("letter_5").style.display = "block";
-}
-function menu6(button) {
-    hideCurrentPage(button.parentElement);
-    document.getElementById("letter_6").style.display = "block";
-}
-function menu7(button) {
-    hideCurrentPage(button.parentElement);
-    document.getElementById("letter_7").style.display = "block";
-}
-function menu8(button) {
-    hideCurrentPage(button.parentElement);
-    document.getElementById("letter_8").style.display = "block";
-}
-function menu9(button) {
-    hideCurrentPage(button.parentElement);
-    document.getElementById("letter_9").style.display = "block";
-}
-function menu10(button) {
-    hideCurrentPage(button.parentElement);
-    document.getElementById("letter_10").style.display = "block";
-}
-function menu11(button) {
-    hideCurrentPage(button.parentElement);
-    document.getElementById("letter_11").style.display = "block";
-}
-function menu12(button) {
-    hideCurrentPage(button.parentElement);
-    document.getElementById("letter_12").style.display = "block";
-}
-function menu13(button) {
-    hideCurrentPage(button.parentElement);
-    document.getElementById("letter_13").style.display = "block";
-}
-function menu14(button) {
-    hideCurrentPage(button.parentElement);
-    document.getElementById("letter_14").style.display = "block";
-}
-function menu15(button) {
-    hideCurrentPage(button.parentElement);
-    document.getElementById("letter_15").style.display = "block";
-}
-function menu16(button) {
-    hideCurrentPage(button.parentElement);
-    document.getElementById("letter_16").style.display = "block";
-}
-function menu17(button) {
-    hideCurrentPage(button.parentElement);
-    document.getElementById("letter_17").style.display = "block";
-}
-function menu18(button) {
-    hideCurrentPage(button.parentElement);
-    document.getElementById("letter_18").style.display = "block";
-}
+
 function begging(button)
 {
     hideCurrentPage(button.parentElement);
